a506636e7200e81c7577d3da0042b6ca7e5b5826
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-26 05:09:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,7 +25,29 @@ end
 def using_uniq(a)
   a.uniq()
 end
-def using_flatten()
+def using_flatten(a)
+  a.faltten()
+end
+def using_delete( a, b)
+  a.delete(b)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   
   
